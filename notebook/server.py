88b7f1a7b7ac8aa321dcd982b91eab997e14a28c
HJ Kim <kode.hjkim@gmail.com>
2024-06-09 23:38:16 +0100

    Move app configuration to config.json.

--- notebook/server.py
+++ notebook/server.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask
 from version import get_version
 
@@ -11,9 +12,11 @@ def home():
 
 
 if __name__ == '__main__':
-    print(f'** Notebook server version: {get_version(os.path.join("notebook", "__init__.py"))}')
+    app.config.from_file('config.json', load=json.load)
 
+    print(f'** Notebook server version: {get_version(os.path.join("notebook", "__init__.py"))}')
     app.run(
-        host='0.0.0.0',
-        port=5000
+        host=app.config["FLASK_HOST"],
+        port=app.config["FLASK_PORT"],
+        debug=app.config["DEBUG"]
     )
