a04394d6e9c9b415ca02d095fae3e541167991bc
HJ Kim <kode.hjkim@gmail.com>
2024-06-10 16:07:19 +0100

    Add missing comment and  warning for maintaining db connection.

--- notebook/server.py
+++ notebook/server.py
@@ -5,6 +5,9 @@ from version import get_version
 from connection import connect_to_db
 
 app = Flask(__name__)
+
+# TODO/NOTE: WARNING - a temporary hack for development due to time limit.
+#   search and refer to solutions for "best practices for persistent database connections with Flask..."
 connection = connect_to_db()
 
 
--- notebook/version.py
+++ notebook/version.py
@@ -1,4 +1,11 @@
 def get_version(version_file_name: str = "notebook/__init__.py") -> str:
+    """
+    Gets the version string from a file.
+    :param version_file_name: File name containing the version info.
+    :return: Version string
+    :param version_file_name:
+    :return:
+    """
     with open(version_file_name, 'r') as f:
         version_file = f.read()
         # TODO: a quick hack. use a regex to verify the format. write a test for it.
